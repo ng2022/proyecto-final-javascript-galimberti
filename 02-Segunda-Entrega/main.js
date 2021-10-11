abd9f39aa68299a8d37e516ace26aab14afa22a3
ng2022 <n.galimberti@gmail.com>
2021-10-11 07:44:49 +0200

    Segunda entrega actualizada

--- 02-Segunda-Entrega/main.js
+++ 02-Segunda-Entrega/main.js
@@ -1,60 +1,41 @@
-// let datosDiaNino = document.getElementById('datosAlmuerzo');
-// datosDiaNino.addEventListener('submit', validarDatos);
-
-// function validarDatos(e) {
-//     e.preventDefault();
-//     let datosAlmuerzo = e.target
-//     console.log(datosAlmuerzo.children[0].value);
-// }
+// Data del dia actual
+let diaActual = document.getElementById('diaActual');
+diaActual.textContent = new Date();
+let diaValue = diaActual.innerHTML;
 
 
-let btn = document.querySelector('#btnUno');
+// Boton Guardar todos los datos
+let buttonGuardar = document.getElementById('button');
+buttonGuardar.addEventListener('click', guardarTodo);
 
-btn.onclick = function () {
-let rbs = document.querySelectorAll('input[name="almuerzo"]');
-let selectedValue;
-for (let rb of rbs) {
-    if (rb.checked) {
-        selectedValue = rb.value;
-        break;
+function guardarTodo () {
+    let containerComidas = document.getElementsByClassName('input-comidas');
+    let horaToma = document.getElementsByClassName("input-hora-toma-leche");
+    let mlToma = document.getElementsByClassName("input-ml-toma-leche");
+    let dia1 = [];
+
+    // Hora toma de leche
+    for (i=0; i<horaToma.length; i++) {
+        if (horaToma[i].value) {
+            console.log(horaToma[i].id + horaToma[i].value);
+            dia1.push({tipologia: horaToma[i].id, hora: horaToma[i].value, toma: horaToma[i].name,  dia: diaValue})
+        }
     }
-}
 
-console.log('Almuerzo ' + selectedValue);
-
-}
-
-
-
-let btnDos = document.querySelector('#btnDos');
-
-btnDos.onclick = function () {
-let rbs = document.querySelectorAll('input[name="primer-plato"]');
-let selectedValue;
-for (let rb of rbs) {
-    if (rb.checked) {
-        selectedValue = rb.value;
-        break;
+    // Ml toma de leche
+    for (i=0; i<mlToma.length; i++) {
+        if (mlToma[i].value) {
+            console.log(mlToma[i].id + mlToma[i].value);
+            dia1.push({tipologia: mlToma[i].id, ml: mlToma[i].value, toma: mlToma[i].name, dia: diaValue})
+        }
     }
-}
 
-console.log('Primer plato ' + selectedValue);
-
-}
-
-
-let btnTres = document.querySelector('#btnTres');
-
-btnTres.onclick = function () {
-let rbs = document.querySelectorAll('input[name="segundo-plato"]');
-let selectedValue;
-for (let rb of rbs) {
-    if (rb.checked) {
-        selectedValue = rb.value;
-        break;
+    // Comidas
+    for (i=0; i<containerComidas.length; i++) {
+        if (containerComidas[i].checked) {
+            console.log(containerComidas[i].name + containerComidas[i].value);
+            dia1.push({portada: containerComidas[i].name, cantidad: containerComidas[i].value, dia: diaValue})
+        }
     }
-}
-
-console.log('Segundo plato ' + selectedValue);
-
+    localStorage.setItem('dia1', JSON.stringify(dia1));
 }
